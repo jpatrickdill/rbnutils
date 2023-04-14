f6fdbf59826f48a1c9b7fc71dc5ce408f44d7f8a
Patrick Dill <jamespatrickdill@gmail.com>
2023-04-13 20:16:02 -0400

    Update defer.ts

--- src/hooks/defer.ts
+++ src/hooks/defer.ts
@@ -8,21 +8,15 @@ export function useDeferUntil<T>(value: T, condition: boolean | (() => boolean))
 
     const [state, setState] = useState<T>(value);
 
-    // I used useMemo instead of useEffect because I don't want updates that
-    // should be immediate to be delayed by one render cycle.
-    // The setState inside this useMemo should be okay because the state
-    // being set is not a dependency and can't cause an infinite loop on its own
-    useMemo(() => {
-        if (state !== value) {
-            if (condition instanceof Function) {
-                condition = condition();
-            }
-
-            // if not using TS, users may pass any value as condition,
-            // and it will get evaluated as boolean
-            if (condition) setState(value);
+    if (state !== value) {
+        if (condition instanceof Function) {
+            condition = condition();
         }
-    }, [value, condition]);
+
+        // if not using TS, users may pass any value as condition,
+        // and it will get evaluated as boolean
+        if (condition) setState(value);
+    }
 
     return state;
 }
